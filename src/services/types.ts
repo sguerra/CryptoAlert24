3edba924c79020843dba00feb29678947cf058da
Sergio Guerra <sergioguerra1991@gmail.com>
2022-05-14 00:31:54 -0500

    Draft: Types definition

---
+++ src/services/types.ts
@@ -0,0 +1,100 @@
+type CryptoAssetLink = {
+  name: string
+  link: string
+}
+
+export type CryptoAssetProfile = {
+  general: {
+    overview: {
+      is_verified: boolean
+      tagline: string
+      category: string
+      sector: string
+      tags: string
+      project_details: string
+      official_links: CryptoAssetLink[]
+    }
+  }
+  technology: {
+    overview: {
+      technology_details: string
+      client_repositories: (CryptoAssetLink & {license_type: string})[]
+    }
+  }
+  governance: {
+    overview: {
+      governance_details: string
+    }
+  }
+}
+
+type CryptoAssetOHLVC = {
+  open: number
+  high: number
+  low: number
+  close: number
+  volume: number
+}
+
+type CryptoAssetMarketData = {
+  price_usd: number
+  price_btc: number
+  volume_last_24_hours: number
+  real_volume_last_24_hours: number
+  volume_last_24_hours_overstatement_multiple: number
+  percent_change_usd_last_24_hours: number
+  percent_change_btc_last_24_hours: number
+  ohlcv_last_1_hour: CryptoAssetOHLVC
+  ohlcv_last_24_hour: CryptoAssetOHLVC
+}
+
+type CryptoCommon = {
+  id: string
+  symbol: string
+  name: string
+  slug: string
+}
+
+export type CryptoAssetMetrics = CryptoCommon & {
+  market_data: CryptoAssetMarketData
+  supply: {
+    y_2050: number
+    y_2050_percent_issued: number
+    supply_yplus_10: number
+    y_plus10_issued_percent: number
+    liquid: number
+    circulating: number
+    stock_to_flow: number
+  }
+  blockchain_stats_24_hours: {
+    transaction_volume: number
+    nvt: number
+    sum_of_fees: number
+    median_tx_value: number
+    median_tx_fee: number
+    count_of_active_addresses: number
+    count_of_tx: number
+    count_of_payments: number
+    new_issuance: number
+    average_difficulty: number
+    kilobytes_added: number
+    count_of_blocks_added: number
+  }
+  all_time_high: {
+    price: number
+    at: string
+    days_since: number
+    percent_down: number
+  }
+  roi_data: {
+    percent_change_last_1_week: number
+    percent_change_last_1_month: number
+    percent_change_last_3_months: number
+    percent_change_last_1_year: number
+  }
+}
+
+export type CryptoAsset = CryptoCommon & {
+  profile: CryptoAssetProfile
+  metrics: CryptoAssetMetrics
+}
--- src/views/CryptoPortfolio.tsx
+++ src/views/CryptoPortfolio.tsx
@@ -1,20 +1,14 @@
 import React, {useCallback, useEffect, useMemo, useState} from 'react'
 import type {FunctionComponent} from 'react'
 
-import {
-  View,
-  TextInput,
-  FlatList,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Image,
-} from 'react-native'
+import {View, TextInput, Text, StyleSheet} from 'react-native'
 import Assets from '../services/assets'
 
 import Config from 'react-native-config'
+import {CryptoList} from '../components/CryptoList'
+import type {CryptoAsset} from '../services/types'
 
-function filterAssets(assetList: object[], assetFilter: string = '') {
+function filterAssets(assetList: CryptoAsset[], assetFilter: string = '') {
   if (assetFilter === '') {
     return assetList
   }
@@ -35,7 +29,7 @@ function filterAssets(assetList: object[], assetFilter: string = '') {
 export const CryptoPortfolio: FunctionComponent = ({navigation}) => {
   const [count, setCount] = useState(0)
   const [page, setPage] = useState(1)
-  const [assets, setAssets] = useState([])
+  const [assets, setAssets] = useState<CryptoAsset[]>([])
   const [assetFilter, setAssetFilter] = useState('')
 
   const service = useMemo(() => {
@@ -77,7 +71,7 @@ export const CryptoPortfolio: FunctionComponent = ({navigation}) => {
   }, [getAllAssets, page])
 
   const itemOnPressHandler = useCallback(
-    itemId => {
+    (itemId: string) => {
       navigation.navigate('Details', {id: itemId})
     },
     [navigation],
@@ -110,48 +104,10 @@ export const CryptoPortfolio: FunctionComponent = ({navigation}) => {
         placeholderTextColor="white"
         onChangeText={searchTextHandler}
       />
-      <FlatList
+      <CryptoList
         onEndReached={endOfListReachedHandler}
-        style={styles.list}
-        renderItem={({item}) => {
-          const imgSourceURI = `${Config.API_IMG_URI}/${item.id}/32.png?v=2`
-
-          const {market_data} = item.metrics
-
-          const price_usd = Math.trunc(market_data.price_usd * 100) / 100
-          const percent_change_usd_last_24_hours =
-            Math.trunc(market_data.percent_change_usd_last_24_hours * 100) / 100
-
-          const deltaStyle = {
-            ...styles.itemDelta,
-            ...(percent_change_usd_last_24_hours >= 0
-              ? styles.itemDeltaInc
-              : styles.itemDeltaDec),
-          }
-
-          return (
-            <TouchableOpacity
-              style={styles.item}
-              onPress={() => {
-                itemOnPressHandler(item.id)
-              }}>
-              <Image source={{uri: imgSourceURI}} style={styles.itemImage} />
-              <View style={styles.itemName}>
-                <Text style={styles.itemText}>{item.symbol}</Text>
-                <Text style={styles.itemSubtitle}>{item.name}</Text>
-              </View>
-              <Text style={{...styles.itemText, ...styles.itemPrice}}>
-                {price_usd}
-              </Text>
-              <View style={deltaStyle}>
-                <Text style={{...styles.itemText, ...styles.itemPercent}}>
-                  {percent_change_usd_last_24_hours} %
-                </Text>
-              </View>
-            </TouchableOpacity>
-          )
-        }}
-        data={filteredAssets}
+        onItemPressed={itemOnPressHandler}
+        assets={filteredAssets}
       />
       <TempDebugSection />
     </View>
@@ -172,60 +128,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#666',
     color: 'white',
     fontSize: 18,
-  },
-  list: {},
-  item: {
-    width: '100%',
-    height: 50,
-    color: 'white',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: '#333',
-    marginTop: 5,
-    borderRadius: 10,
-  },
-  itemName: {
-    flexGrow: 1,
-  },
-  itemText: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  itemSubtitle: {
-    color: '#ddd',
-    fontSize: 12,
-  },
-  itemPrice: {
-    textAlign: 'right',
-    fontSize: 18,
-    fontWeight: 'bold',
-    margin: 12,
-  },
-  itemImage: {
-    height: 28,
-    width: 28,
-    margin: 7,
-  },
-  itemDelta: {
-    borderRadius: 10,
-    flexGrow: 0,
-    minWidth: 80,
-    margin: 8,
-    textAlign: 'center',
-  },
-  itemDeltaInc: {
-    backgroundColor: '#36c252',
-  },
-  itemDeltaDec: {
-    backgroundColor: '#e04638',
-  },
-  itemPercent: {
-    textAlign: 'center',
-    fontSize: 16,
-    fontWeight: 'bold',
-    margin: 5,
   },
   tempDebug: {backgroundColor: 'white'},
 })
